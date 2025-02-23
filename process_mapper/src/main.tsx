c641df01b428caaf009f952dbb317fb0170f189b
thomaswfaulkner <64921609+thomaswfaulkner@users.noreply.github.com>
2025-02-23 17:25:20 -0600

    Update main.tsx

--- process_mapper/src/main.tsx
+++ process_mapper/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import IntegratedSuite from './App.tsx';
-import './index.css';
+import IntegratedSuite from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <IntegratedSuite />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
