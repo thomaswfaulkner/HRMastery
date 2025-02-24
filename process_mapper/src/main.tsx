cc4fceaafd551fa6b5999250208b4ec13f457f0e
thomaswfaulkner <64921609+thomaswfaulkner@users.noreply.github.com>
2025-02-23 20:12:30 -0600

    Delete process_mapper directory

--- process_mapper/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import IntegratedSuite from './App';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <IntegratedSuite />
-  </React.StrictMode>
-);
